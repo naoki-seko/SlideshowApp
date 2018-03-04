8a86d4b99b5666e98ac9c30ceabab95c58c869e8
Kimi Lab 岳研 <kimilabo@kimi-no-MacBook-Air.local>
2018-03-04 09:37:59 +0900

    自動送り中は進むボタンと戻るボタンをタップ不可にした。 再生ボタンをタップすると停止ボタンになり、停止ボタンをタップすると再生ボタンになるようにした。 停止ボタンをタップすると自動送りが止まり、進むボタンと戻るボタンがタップ可能になるようにした。

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -12,6 +12,11 @@ class ViewController: UIViewController {
 
     let photos = ["IMG_9364.JPG","IMG_9365.JPG","IMG_9366.JPG","IMG_9367.JPG"]
     
+    
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var startStopButton: UIButton!
+    
     //自動で写真を表示するためのタイマー
     var timer: Timer!
     //表示する番号用の数字
@@ -20,13 +25,6 @@ class ViewController: UIViewController {
     //タイマー用の関数
     @objc func updateTimer(timer: Timer) {
         print("onTimer")
-        //表示している画像の番号から名前を取り出し
-        let name = photos[dispImageNo]
-        //画像の読み込み
-        let myImage: UIImage = UIImage(named: name)!
-        //ImageViewに読み込んだ画像をセット
-        myImageView.image = myImage
-        
         //表示している画像の番号を１増やす
         dispImageNo += 1
         //範囲外の場合最初か最後の画像を表示
@@ -36,6 +34,12 @@ class ViewController: UIViewController {
         if dispImageNo > 3 {
             dispImageNo = 0
         }
+        //表示している画像の番号から名前を取り出し
+        let name = photos[dispImageNo]
+        //画像の読み込み
+        let myImage: UIImage = UIImage(named: name)!
+        //ImageViewに読み込んだ画像をセット
+        myImageView.image = myImage
     }
    //進むボタンを押したときの動作
     @IBAction func nextButton(_ sender: Any) {
@@ -74,16 +78,24 @@ class ViewController: UIViewController {
     @IBAction func startStopButton(_ sender: Any) {
         //再生
         if self.timer == nil{
+            startStopButton.setTitle("停止", for: .normal)
             self.timer = Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
-        }
+            nextButton.setTitleColor(UIColor.black, for: .normal)
+            backButton.setTitleColor(UIColor.black, for: .normal)
+            nextButton.isEnabled = false
+            backButton.isEnabled = false
+        }//停止
         else if self.timer != nil{
+            startStopButton.setTitle("再生", for: .normal)
             self.timer.invalidate()
             self.timer = nil
+            nextButton.setTitleColor(UIColor.blue, for: .normal)
+            backButton.setTitleColor(UIColor.blue, for: .normal)
+            nextButton.isEnabled = true
+            backButton.isEnabled = true
         }
         
-    
-        
-        
+       
     }
     
     @IBOutlet weak var myImageView: UIImageView!
@@ -94,6 +106,9 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        startStopButton.setTitleColor(UIColor.blue, for: .normal)
+        nextButton.setTitleColor(UIColor.blue, for: .normal)
+        backButton.setTitleColor(UIColor.blue, for: .normal)
         //表示している画像の番号から名前を取り出し
         let name = photos[dispImageNo]
         //画像の読み込み
